28d9b84719042a2e5353f68502f979da4aed9c24
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2016-02-28 15:54:32 +0100

    Connected views from layout to the code, added on button click listener

--- HI-View-Demo/app/src/main/java/com/hyperisland/hi_view_demo/MainActivity.java
+++ HI-View-Demo/app/src/main/java/com/hyperisland/hi_view_demo/MainActivity.java
@@ -2,12 +2,44 @@ package com.hyperisland.hi_view_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button myButton;
+    private View awesomeView;
+
+    private static int[] colors = {
+            R.color.red,
+            R.color.orange,
+            R.color.yellow,
+            R.color.green,
+            R.color.cyan,
+            R.color.blue,
+            R.color.purple,
+    };
+
+    private Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        random = new Random();
+        initUI();
+    }
+
+    private void initUI() {
+        awesomeView = findViewById(R.id.awesomeView);
+        myButton = (Button) findViewById(R.id.myButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                awesomeView.setBackgroundColor(MainActivity.this.getResources().getColor(colors[random.nextInt(colors.length)]));
+            }
+        });
     }
 }
